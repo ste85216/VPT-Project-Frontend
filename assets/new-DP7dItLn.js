6858128822f44daf25a65b0a344f27f40b83151f
ste85216 <ste85213@gmail.com>
2024-08-20 05:35:10 +0000

    Deploying to gh-pages from @ ste85216/VPT-Project-Frontend@31776bb737b3c060f0c9d68c8422de268c829ae3 🚀

--- assets/new-DP7dItLn.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./route-block-B_A1xBdJ.js";import{b as o,o as t}from"./index-D9y08I5N.js";const n={__name:"new",setup(r){return(c,a)=>(t(),o("h2",null,"新品上市"))}};typeof e=="function"&&e(n);export{n as default};
