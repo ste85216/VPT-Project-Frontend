3d3fbbe482c20f18e005a6f7fd37f1a15ff410cb
ste85216 <ste85213@gmail.com>
2024-08-23 03:36:03 +0000

    Deploying to gh-pages from @ ste85216/VPT-Project-Frontend@eb6666c25f9c870235d41b051505bf6ea174a1c1 🚀

--- assets/new-BXIlH4wT.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./route-block-B_A1xBdJ.js";import{o,b as t}from"./index-U53tKImo.js";const n={__name:"new",setup(r){return(c,a)=>(o(),t("h2",null,"新品上市"))}};typeof e=="function"&&e(n);export{n as default};
